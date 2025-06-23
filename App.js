7262a417b286cb13818a2d39af650030acdf7c49
Umesh Mane <umeshmane575@gmail.com>
2025-06-23 15:46:51 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,22 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am H1 tage"),
+    React.createElement("h2", {}, "i am h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am H1 tage"),
+    React.createElement("h2", {}, "i am h2 tag"),
+  ]),
+]);
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Hello world form React"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
+
+// JSX  that exist
